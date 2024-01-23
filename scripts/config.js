11efbb02fc4e58b505b08273a71205b5e930135e
Ric <67565057+RA86-dev@users.noreply.github.com>
2024-01-23 16:04:42 -0600

    Update config.js

--- scripts/config.js
+++ scripts/config.js
@@ -1,7 +1,7 @@
 // config.js
 
 // Function to get API key
-export const getApiKey = () => 'ec1298750d0c4702871132901240601';
+export const getApiKey = () => btoa(`yÍv÷ÎùÑÝ\x1Cã½6ó½ußot×n4ëM`)
 
 
 // Other configuration variables and functions can be added here
