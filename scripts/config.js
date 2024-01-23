7720dfb2ef397f0a3b50acb6ff18906bc9e45d7c
Ric <67565057+RA86-dev@users.noreply.github.com>
2024-01-23 16:13:15 -0600

    Update config.js

--- scripts/config.js
+++ scripts/config.js
@@ -1,7 +1,7 @@
 // config.js
-
+function decodeApiKey(e){try{if("string"!=typeof e)throw Error("Encoded API key must be a string");let r=decodeURIComponent(escape(atob(e)));return r}catch(t){return console.error("Error decoding API key:",t.message),null}}
 // Function to get API key
-export const getApiKey = () => btoa(`yÍv÷ÎùÑÝ\x1Cã½6ó½ußot×n4ëM`)
+export const getApiKey = () => decodeApiKey(`ZWMxMjk4NzUwZDBjNDcwMjg3MTEzMjkwMTI0MDYwMQ==`)
 
 
 // Other configuration variables and functions can be added here
